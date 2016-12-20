dfe46cb6234a7fd069f92bc9e488fa38fd192109
David van Geest <dvangeest@pagerduty.com>
2016-12-20 18:03:43 -0500

    Don't double-publish.

--- .travis.release.sh
+++ .travis.release.sh
@@ -26,7 +26,7 @@ GIT_TAG=v$RELEASE_VER
 echo "Conditionally publishing release and cutting git tag..."
 
 if ! git ls-remote --exit-code origin refs/tags/$GIT_TAG; then
-  if [ "${TRAVIS_PULL_REQUEST}" == 'false' -a "${TRAVIS_JDK_VERSION}" == 'oraclejdk8' ]; then
+  if [ "${TRAVIS_PULL_REQUEST}" == 'false' -a "${TRAVIS_JDK_VERSION}" == 'oraclejdk8' -a "${CASS}" == 'cassandra2' ]; then
     sbt -Dsbt.global.base=/home/travis/.sbt ++${TRAVIS_SCALA_VERSION} publish &&
     git tag -a $GIT_TAG -m "Release version $RELEASE_VER" &&
     git push origin $GIT_TAG
